b03002de8dfc9fe04a4c006c9a304f78c6059166
Hiraina <hei.hiraina@gmail.com>
2025-12-15 14:32:47 +0300

    add schema.sql

---
+++ DB/schema.sql
@@ -0,0 +1,19 @@
+CREATE TYPE positions_enum AS ENUM ('GK', 'DEF', 'MIDF', 'STR');
+
+CREATE TYPE continents_enum AS ENUM ('AFRICA', 'EUROPA', 'ASIA', 'AMERICA');
+
+CREATE TABLE "Team" (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    continent continents_enum NOT NULL
+);
+
+
+CREATE TABLE "Player" (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    "position" positions_enum NOT NULL,
+    id_team INT,
+    CONSTRAINT fk_team FOREIGN KEY (id_team) REFERENCES "Team"(id) ON DELETE CASCADE
+);
