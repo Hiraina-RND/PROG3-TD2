cca2d95be34cf11db0f43c9fa4f0fb3483c02d00
Hiraina <hei.hiraina@gmail.com>
2025-12-15 14:47:06 +0300

    add Team class

---
+++ src/main/java/Team.java
@@ -0,0 +1,66 @@
+import java.util.List;
+import java.util.Objects;
+
+public class Team {
+
+    public enum Continents_enum {
+        AFRICA,
+        EUROPA,
+        ASIA,
+        AMERICA
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return id == team.id && Objects.equals(name, team.name) && continent == team.continent && Objects.equals(players, team.players);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, continent, players);
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", continent=" + continent +
+                ", players=" + players +
+                '}';
+    }
+
+    private final int id;
+    private final String name;
+    private final Continents_enum continent;
+    private final List<Player> players;
+
+    private Integer getPlayersCount() {
+        return players.size();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Continents_enum getContinent() {
+        return continent;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Team(int id, String name, Continents_enum continent, List<Player> players) {
+        this.id = id;
+        this.name = name;
+        this.continent = continent;
+        this.players = players;
+    }
+}
