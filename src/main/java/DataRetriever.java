a7e7f36b9e821cf00f92c0002c6eedab893d249f
Hiraina <hei.hiraina@gmail.com>
2025-12-18 17:30:43 +0300

    feat: implement findTeamById and required helper methods in DataRetriever

---
+++ src/main/java/DataRetriever.java
@@ -0,0 +1,112 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRetriever {
+    private Team mapToTeam(ResultSet resultSet) throws SQLException {
+        return new Team(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                Team.ContinentEnum.valueOf(resultSet.getString("continent")),
+                findAllPlayerByIdTeam(resultSet.getInt("id"))
+        );
+    }
+
+    private Player mapToPlayer(ResultSet resultSet) throws SQLException {
+        return new Player(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getInt("age"),
+                Player.PlayerPositionEnum.valueOf(resultSet.getString("position")),
+                findTeamByIdWithoutPlayers(resultSet.getInt("id_team"))
+        );
+    }
+
+    private Team findTeamByIdWithoutPlayers(Integer id) {
+        DBConnection dbConnection = new DBConnection();
+
+        String SQL = """
+            SELECT id, name, continent
+            FROM "Team"
+            WHERE id = ?
+            """;
+
+        try (
+                Connection connection = dbConnection.getDBConnection();
+                PreparedStatement ps = connection.prepareStatement(SQL)
+        ) {
+            ps.setInt(1, id);
+
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    return new Team(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            Team.ContinentEnum.valueOf(resultSet.getString("continent")),
+                            null
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    private List<Player> findAllPlayerByIdTeam(Integer idTeam) {
+        DBConnection dbConnection = new DBConnection();
+        List<Player> players = new ArrayList<>();
+
+        String SQL = """
+                SELECT id, name, age, "position", id_team
+                FROM "Player"
+                WHERE id_team = ?
+                """;
+
+        try(
+                Connection connection = dbConnection.getDBConnection();
+                PreparedStatement ps = connection.prepareStatement(String.valueOf(SQL))
+        ) {
+            ps.setInt(1, idTeam);
+
+            try(
+                    ResultSet resultSet = ps.executeQuery()
+            ) {
+                while (resultSet.next()) {
+                    players.add(mapToPlayer(resultSet));
+                }
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Error executing query", e);
+        }
+        return players;
+    }
+
+    public Team findTeamById(Integer id) {
+        DBConnection dbConnection = new DBConnection();
+        String SQL = """
+                SELECT id, name, continent
+                FROM "Team"
+                WHERE id = ?
+                """;
+        Team team = null;
+
+        try (
+                Connection connection = dbConnection.getDBConnection();
+                PreparedStatement ps = connection.prepareStatement(String.valueOf(SQL))
+        ){
+            ps.setInt(1, id);
+
+            try (
+                    ResultSet resultSet = ps.executeQuery()
+            ) {
+                if (resultSet.next()) {
+                    team = mapToTeam(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing query", e);
+        }
+        return team;
+    }
+}
