e41b84c1e057d1d9510eef9572b3238f49bd3481
Hiraina <hei.hiraina@gmail.com>
2026-01-07 11:33:02 +0300

    add close method for DBConnection class

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -18,4 +18,14 @@ public class DBConnection {
             throw new RuntimeException(e);
         }
     }
+
+    public void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
